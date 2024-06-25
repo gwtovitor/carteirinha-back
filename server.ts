9c62f1087a84b0b7e04371a33182768afdb1fc94
gwtovitor <gwtovitorpw@gmail.com>
2024-06-25 10:26:02 -0300

    create server

--- server.ts
+++ server.ts
@@ -1,88 +1,51 @@
-// import * as http from 'http'
-// import AppInstance from './app'
+import * as dotenv from 'dotenv';
+dotenv.config();
+import * as http from 'http';
+import appInstance from 'app';
+const app = appInstance.app;
+const port = normalizePort(process.env.PORT || '8081');
+app.set('port', port);
 
-// const app = AppInstance.app
-// const port = normalizePort(process.env.PORT || '8081')
-// app.set('port', port)
+const server = http.createServer(app);
 
-// const server = http.createServer(app)
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
-// //Socket
-// import { Server } from 'socket.io'
-// import EscapeLive from '@entities/games/escape-live/sockets/escape-live'
-// import SalaMagia from '@entities/games/sala-magia/sala-magia'
+function normalizePort(val: string) {
+    const port = parseInt(val, 10);
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+}
 
-// const io = new Server(server, {
-//     path: '/socket.io',
-//     cors: {
-//         origin: '*',
-//         credentials: true,
-//         methods: ['GET', 'POST'],
-//     },
-// })
+function onError(error: NodeJS.ErrnoException): void {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
 
-// // io.on('connection', (socket: any) => {})
+    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
-// SalaMagia(server)
-// EscapeLive(server)
+    switch (error.code) {
+        case 'EACCES':
+            console.error(`${bind} requires elevated privileges`);
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(`${bind} is already in use`);
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
 
-// server.listen(port)
-// server.on('error', onError)
-// server.on('listening', onListening)
-
-// export { io }
-
-// /**
-//  * Normalize a port into a number, string, or false.
-//  */
-
-// function normalizePort(val: any) {
-//     const port = parseInt(val, 10)
-
-//     if (isNaN(port)) {
-//         // named pipe
-//         return val
-//     }
-
-//     if (port >= 0) {
-//         // port number
-//         return port
-//     }
-
-//     return false
-// }
-
-// /**
-//  * Event listener for HTTP server "error" event.
-//  */
-
-// function onError(error: any) {
-//     if (error.syscall !== 'listen') {
-//         throw error
-//     }
-
-//     const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port
-
-//     // handle specific listen errors with friendly messages
-//     switch (error.code) {
-//         case 'EACCES':
-//             console.error(bind + ' requires elevated privileges')
-//             process.exit(1)
-
-//         case 'EADDRINUSE':
-//             console.error(bind + ' is already in use')
-//             process.exit(1)
-
-//         default:
-//             throw error
-//     }
-// }
-
-// /**
-//  * Event listener for HTTP server "listening" event.
-//  */
-
-// function onListening() {
-//     const addr = server.address()
-//     const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr?.port
-// }
+function onListening(): void {
+    const addr = server.address();
+    const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr?.port}`;
+    console.log(`Listening on ${bind}`);
+}
