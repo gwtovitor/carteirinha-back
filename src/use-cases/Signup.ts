dd40c15613bcb86a4b7bfe6e39d43c1d9e883c22
gwtovitor <gwtovitorpw@gmail.com>
2024-06-25 10:27:41 -0300

    create signup UC

--- src/use-cases/Signup.ts
+++ src/use-cases/Signup.ts
@@ -1,29 +1,33 @@
-
-import UserRepo from "src/repo/UserRepo"
-import User from "./User.do"
+import UserRepo from 'src/repo/UserRepo';
+import User from './User.do';
 
 export default class Signup {
-    private repo: UserRepo
+	private repo: UserRepo;
 
-    constructor(repo: UserRepo) {
-        this.repo = repo
-    }
+	constructor(repo: UserRepo) {
+		this.repo = repo;
+	}
 
-    async execute(input: input) {
-        const user = User.create(input)
+	async execute(input: input) {
 
-        const found = await this.repo.findByEmail(user.getEmail())
-        if (found != undefined) {
-            throw new Error('A user with this email address already exists')
-        }
+		const validity = new Date();
+		validity.setFullYear(validity.getFullYear() + 1);
+		input.validity = validity
 
-        await this.repo.create(user)
-    }
+		const user = User.create(input);
+		const found = await this.repo.findByEmail(user.getEmail());
+		if (found != undefined) {
+			throw new Error('A user with this email address already exists');
+		}
+		await this.repo.create(user);
+		return;
+	}
 }
 
 type input = {
-        email: string,
-        name: string,
-        password: string,
-        validity: Date
-}
+	email: string;
+	name: string;
+	password: string;
+	validity: Date;
+	photo: string;
+};
