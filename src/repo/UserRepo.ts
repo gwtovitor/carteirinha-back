2feb91300f121adcb31e5bbbf37dd110b7606838
gwtovitor <gwtovitorpw@gmail.com>
2024-06-25 10:28:11 -0300

    update user repo interface

--- src/repo/UserRepo.ts
+++ src/repo/UserRepo.ts
@@ -2,5 +2,7 @@ import User from "src/use-cases/User.do"
 
 export default interface UserRepo {
     create(input: User): Promise<void>
+    update(user: User): Promise<void>
     findByEmail(email: string): Promise<User | undefined>
+    findById(id: string): Promise<User | undefined>
 }
