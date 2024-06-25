efa6305300d04c344b91d2cccfaea1b46b1b51c3
gwtovitor <gwtovitorpw@gmail.com>
2024-06-25 10:26:24 -0300

    create route class

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+import express, { Router } from 'express'
+import UserRoutes from './api/user.routes'
+
+export class routes {
+
+    private router: Router = express.Router()
+
+    public getAllRoutes() {
+        console.log('🛣️  Routes')
+
+        this.router.use(UserRoutes())
+
+        return this.router
+    }
+}
+
+export default routes
