b5e6a78d073edfb42a82634d7c13314fd7c1c225
gwtovitor <gwtovitorpw@gmail.com>
2024-06-25 10:26:43 -0300

    create user routes

---
+++ src/api/user.routes.ts
@@ -0,0 +1,19 @@
+import express, { Router } from 'express';
+import Container from 'typedi';
+import { auth } from 'src/middleware/auth';
+import UserController from './user.controller';
+
+function routes() {
+	const router: Router = express.Router();
+	const controller = new UserController();
+
+	router.post(`/signup`, controller.signup);
+
+	router.post(`/login`, controller.login);
+
+	router.put(`/user/update/:id`, auth, controller.update);
+
+	return router;
+}
+
+export default routes;
